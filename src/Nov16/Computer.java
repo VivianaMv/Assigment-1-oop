9325900b300f09f4d7f57f9369773a758af69eb9
Viviana <vivianamontoya14@gmail.com>
2023-11-26 10:29:54 -0500

    Parte2

--- src/Nov16/Computer.java
+++ src/Nov16/Computer.java
@@ -8,75 +8,85 @@ package Nov16;
 
 
 public class Computer {
-	
+
 	private static int numOfComputer = 0;
 	private static long serialnumcounter = 100000;
-	
-	
+
 	private String brand;
 	private String model;
 	private long serialnum;
 	private double price;
 
+	public Computer() {
+		brand = "";
+		model = "";
+		price = 0;
+		serialnum = serialnumcounter;
+		serialnumcounter++;
+		numOfComputer++;
+	}
 
-	
-//constructor
-	public Computer(String b,String m, double p) { 
+	// constructor
+	public Computer(String b, String m, double p) {
 		System.out.println("Creating the computer with parameter construtor");
 		brand = b;
 		model = m;
 		price = p;
 		serialnum = serialnumcounter;
-		serialnumcounter++;		
+		serialnumcounter++;
 		numOfComputer++;
 	}
-	
-// (Hint: Accessors & Mutators.)	
+
+	// (Hint: Accessors & Mutators.)
 	public String getBrand() {
 		return brand;
 	}
+
 	public void setBrand(String b) {
 		brand = b;
-}
-	
+	}
+
 	public String getModel() {
 		return model;
 	}
+
 	public void setModel(String m) {
 		model = m;
-	
+
 	}
-	
+
 	public long getSn() {
 		return serialnum;
 	}
+
 	public void setSn(long s) {
 		serialnum = s;
-	}	
-		public double getPrice() {
-			return price;
-		}
-		public void setPrice(double p) {
-			price = p;
-		}
+	}
 
-// Hint: toString() method ==> right click + go to source + Generate toString()		
-		@Override
-		public String toString() {
-			System.out.println("The brand of the computer is: " + brand);
-			System.out.println("The model : " + model);
-			System.out.println("The price is: " + price);
-			System.out.println("The SN: " + serialnum);
-			
-			return "Computer [brand=" + brand + ", model=" + model + ", sn=" + serialnum + ", price=" + price + "]";
-		}	
-		
-		public boolean equals(Computer c) {
-			if(brand == c.brand && model == c.model && price == c.price ) {
-				return true;
-			}else {
-				return false; 
-			}
-		}
-}	
+	public double getPrice() {
+		return price;
+	}
 
+	public void setPrice(double p) {
+		price = p;
+	}
+
+	// Hint: toString() method ==> right click + go to source + Generate toString()
+	@Override
+	public String toString() {
+		System.out.println("The brand of the computer is: " + brand);
+		System.out.println("The model : " + model);
+		System.out.println("The price is: " + "$" + price);
+		System.out.println("The SN: " + serialnum);
+
+		return "Computer [brand=" + brand + ", model=" + model + ", sn=" + serialnum + ", price=" + price + "]";
+	}
+
+	public boolean equals(Computer c) {
+		if (brand == c.brand && model == c.model && price == c.price) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
--- src/Nov16/Demo.java
+++ src/Nov16/Demo.java
@@ -2,128 +2,232 @@ package Nov16;
 
 import java.util.Scanner;
 
-
 public class Demo extends Computer {
+	// Function to find computers with a price lower than the one entered by the
+	// user.
+	// the function need the variable for the price and the array where the
+	// computers are stored
+	public static void findCheaperThan(double priceValue, Computer[] inventory) {
+		boolean found = false;
+		// This is a for that iterates in each index of the array and shows the
+		// characteristics of the
+		// computers that are stored on the inventary
+		for (Computer computer : inventory) {
+			// if the price entered by the user is lower than the price of the computers
+			// display all the computers with lower price
+			if (computer.getPrice() < priceValue) {
+				System.out.println("-----------------------------------");
+				System.out.println(computer);
+				System.out.println("-----------------------------------");
+				System.out.println();
+				found = true;
+			}
+		}
+		if (!found) {
+			System.out.println("No computers found with a price lower than: " + priceValue);
+		}
+	}
+
+	public static void findComputersBy(String brandName, Computer[] inventory) {
+		boolean found = false;
+		for (Computer computer : inventory) {
+			if (computer.getBrand().equalsIgnoreCase(brandName)) {
+				// if the brand entered by the user is the same as one of the stored ones
+				// display computers with that brannd
+				System.out.println("-----------------------------------");
+				System.out.println(computer);
+				System.out.println("-----------------------------------");
+				System.out.println();
+				found = true;
+			}
+		}
+		if (!found) {
+			System.out.println("No computers found with the brand: " + brandName);
+		}
+	}
+
+	// Function to validate passwords
+	private static boolean validatePassword(Scanner scanner) {
+		final int total_attempts = 3;
+		int tries = 0;
+		String password = "password";
+		// the user enters the password, only if he/she doesn't exceeds the total
+		// attempts
+		while (tries < total_attempts) {
+			System.out.println("Please enter your password:");
+			String inputPwd = scanner.nextLine();
+			// if password is correct return true
+			if (inputPwd.equals(password)) {
+				System.out.println("Your password is correct.");
+				return true;
+			} else {
+				// if password is not correct let the user try again
+				System.out.println("Incorrect password. Please try again.");
+				tries++;
+			}
+		}
+		// if user exceeds number of tries close the program
+		System.out.println("Exceeded maximum attempts. Access denied.");
+		return false;
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+		// variables
 		Scanner kb = new Scanner(System.in);
+		Scanner kb2 = new Scanner(System.in);
+		int maxComputer;
+		int index_computer;
+		int index_display;
 		System.out.println("Wellcome to Pargol's Store");
-		System.out.println("Please give me the inventory ");
-		int maxComputer = kb.nextInt();
+		System.out.println("Please give me the inventory: ");
+		maxComputer = kb.nextInt();
 		Computer[] inventory = new Computer[maxComputer];
 		int availableSpace = inventory.length;
-		int choice;
 		int choice1;
-		//Computer  addcomputer = new Computer
-		
+		int choice;
+
+		// MAIN MENU
 		do {
 			System.out.println("What do you want to do? ");
-			System.out.println("1: Enter new computers (password required");
-			System.out.println("2: Change information of a computer (password required ");
+			System.out.println("1: Enter new computers (password required)");
+			System.out.println("2: Change information of a computer (password required)");
 			System.out.println("3: Display all computers by a specific brand ");
 			System.out.println("4: Display all computers under a certain a price. ");
 			System.out.println("5: Quit ");
 			System.out.println("Please enter your choice > ");
-			
 			choice = kb.nextInt();
-			
-			
-			if (choice == 1) { // here is the loop when the user has to type the password
-				
-				Scanner kb1 = new Scanner(System.in);
-				System.out.println("Please give your password");
-				String pwd = kb1.nextLine();
-				int tries = 1;
-				String password = "password";
 
-				while (tries <= 3) {
-				    if (pwd.equals(password)) {
-				        System.out.println("Your password is correct");
-				        System.out.println("how many computers do yu want to add?");	
-				        Scanner kb2 = new Scanner(System.in);
-				        int newcomputers = kb2.nextInt();
-				        
-				        if(newcomputers <= availableSpace) {
-				        	for (int i = 0; i< newcomputers; i++) {
-				        		
-				        	}
-				        	availableSpace -= newcomputers;
-				        }else {
-				        	System.out.println("You can not add many computers");
-				        	break;}
-				    } else {
-				        System.out.println("Your password is incorrect");
-				        tries++;
-				        if (tries <= 3) {
-				            System.out.println("Please try again");
-				            pwd = kb1.nextLine();
-				        	}
-				        }
-				
-				}
-				
-			}
-			
-			
-		
-			else if (choice == 2) { // the user has to type again the password - e.	When option 2 is entered : 
-				
-				Scanner kb2 = new Scanner(System.in);
-				System.out.println("Please give your password");
-				String pwd = kb2.nextLine();
-				int tries = 1;
-				String password = "password";
-
-				while (tries <= 3) {
-				    if (pwd.equals(password)) {
-				        System.out.println("Your password is correct");
-				        break;
-				        
-				    } else {
-				        System.out.println("Your password is incorrect");
-				        tries++;
-				        if (tries <= 3) {
-				            System.out.println("Please try again");
-				            pwd = kb2.nextLine();
-				        	}
-				        }
-				}
-			}
-				else if (choice == 3) { // Then ask the user which attribute they wish to change by displaying the following menu : 
-	
-					do {
-						System.out.println("What information would you like to change ");
-						System.out.println("1: brand ");
-						System.out.println("2: model ");
-						System.out.println("3: SN ");
-						System.out.println("4: price ");
-						System.out.println("5: Quit ");
-						System.out.println("Please enter your choice > ");
-						
-						choice1 = kb.nextInt();
-						
-						if (choice == 5) {
-							System.out.println(" Returning to the main menu ");
-							break;
-						}
-					        	}	while(choice1 !=5); 
-									
+			switch (choice) {
+				case 1:
+					if (validatePassword(kb2)) {
+						System.out.println("How many computers do you want to add?");
+						int newComputers = kb.nextInt();
+						// user only can add computers if it is enough space on the inventary
+						if (newComputers <= availableSpace) {
+							for (int i = 0; i < newComputers; i++) {
+								System.out.println(" ");
+								if (inventory[i] == null) {
+									Computer c = new Computer();
+									System.out.printf("Computer %d - Enter the brand: ", i + 1);
+									c.setBrand(kb.next());
+									System.out.printf("Computer %d - Enter the model: ", i + 1);
+									c.setModel(kb.next());
+									System.out.printf("Computer %d - Enter the price: ", i + 1);
+									c.setPrice(kb.nextDouble());
+									inventory[i] = c;
+									System.out.println();
+								}
 							}
-		
-			
-				else if (choice == 5) {
-					System.out.println("Thanks you for using this sofware ");
+							// reduce the space of the
+							availableSpace -= newComputers;
+						} else {
+							System.out.println("You cannot add that amount of computers.");
+						}
+					}
 					break;
 
-		
-		}
-	
-	}while(choice != 5);
-		
-}
-	
-}
+				case 2:
+					if (validatePassword(kb2)) {
+						System.out.println("which computer number do you wish to update?");
+						// user enters the computer that needs an updated
+						index_display = kb.nextInt();
+						// since indexes in java starts in 0 rest 1
+						index_computer = index_display - 1;
+						// loop where the index needs to equal or bigger than zero
+						// and also it has to not exceed the lenght of the array
+						if (index_computer < inventory.length && index_computer >= 0) {
+							System.out.println("-----------------------------------");
+							System.out.println("This is the PC you want to update:");
+							System.out.printf("Computer #%d is:", index_display);
+							System.out.println();
+							System.out.println("-----------------------------------");
+							inventory[index_computer].toString();
+							System.out.println("-----------------------------------");
+							System.out.println();
+							// menu to update the computer
+							do {
+								System.out.println("What information would you like to change? ");
+								System.out.println("1: brand ");
+								System.out.println("2: model ");
+								System.out.println("3: SN ");
+								System.out.println("4: price ");
+								System.out.println("5: Quit ");
+								System.out.println("Please enter your choice > ");
+								choice1 = kb.nextInt();
+								switch (choice1) {
+									case 1:
+										// get brand and change it with set brand
+										System.out.println("Enter the new brand:");
+										String newBrand = kb.next();
+										inventory[index_computer].setBrand(newBrand);
+										break;
+									case 2:
+										// get model and change it with set model
+										System.out.println("Enter the new model:");
+										String newModel = kb.next();
+										inventory[index_computer].setModel(newModel);
+										break;
+									case 3:
+										// get sn and change it with set sn
+										System.out.println("Enter the new serial number:");
+										long newSerialNumber = kb.nextLong();
+										inventory[index_computer].setSn(newSerialNumber);
+										break;
+									case 4:
+										// get price and change it with set price
+										System.out.println("Enter the new price:");
+										double newPrice = kb.nextDouble();
+										inventory[index_computer].setPrice(newPrice);
+										break;
+									case 5:
+										// quit
+										System.out.println("Returning to the main menu.");
+										break;
+									default:
+										// in case the user types a different option of the existing ones
+										System.out.println("Invalid choice. Please choose again.");
+										break;
+								}
+								// display computer with updated
+								System.out.println("-----------------------------------");
+								System.out.println("Updated Computer Details:");
+								System.out.println(inventory[index_computer]);
+								System.out.println("-----------------------------------");
+							} while (choice1 != 5);
+						} else {
+							System.out.println(
+									"You wish to enter another computer, please try option 1 in the main menu");
+						}
+					}
+					break;
 
+				case 3:
+					// user search for computers of the same brand
+					System.out.println("Enter the brand name to search for:");
+					String searchBrand = kb.next();
+					findComputersBy(searchBrand, inventory);
+					break;
+				case 4:
+					// user search for computers with lower price entered
+					System.out.println("Enter the price value do you want to search:");
+					double searchPrice = kb.nextDouble();
+					findCheaperThan(searchPrice, inventory);
+					break;
+				case 5:
+					// closing the software
+					System.out.println("Thank you for using this software.");
+					break;
+				default:
+					// in case the user types an unexisting option
+					System.out.println("-----------------------------------");
+					System.out.println("Invalid Option!!!, try again");
+					System.out.println("-----------------------------------");
+					System.out.println();
+					break;
+			}
+		} while (choice != 5);
+		// close the program
+		kb.close();
+	}
 
+}
